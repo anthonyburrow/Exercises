a87e570669fe48d0bcfa1121acd3ad7ea3e8254b
anthonyburrow <35277453+anthonyburrow@users.noreply.github.com>
2018-01-12 00:30:24 -0600

    Updated Framework
    Got rid of global dependence by introducing a more consistent class system

--- Game of Life/gol.py
+++ Game of Life/gol.py
@@ -3,9 +3,9 @@ from Tkinter import *
 from threading import Timer
 import math
 
-#######SETTINGS#######
+####### SETTINGS #######
 
-speed = 0.3						# Seconds for each refresh
+interval = 0.3					# Seconds for each refresh
 width = 40						# Number of columns
 height = 40						# Number of rows
 
@@ -15,189 +15,193 @@ window_height = 700				# Window height in px (not counting control button)
 live_color = "#424242"			# Color of living cells
 dead_color = "#bababa"			# Color of dead cells
 
-######################
+button_border_width = 1			# Cell border width in px
 
-root = tk.Tk()
-button_container = tk.Frame(root)
-button_container.grid(row = height + 1, columnspan=width, sticky="wens", padx = 6, pady = 6)
-buttons = []
-alive = []
-is_running = False
-is_clearing = False
+########################
 
-dead_neighbors_checked = [] #compilation & writing purposes
-turn_alive = []
-turn_dead = []
+class Controller:
 
-#Function to change color when a cell is clicked
+	# Initialize
 
-def click(row, col):
-	button = buttons[row][col]
-	coord = [row, col]
+	def __init__(self, _interval, _live_color, _dead_color):
+		self._interval = _interval
+		self._live_color = _live_color
+		self._dead_color = _dead_color
 
-	if coord not in alive:
-		button.configure(bg = live_color)
-		alive.append(coord)
-	elif coord in alive:
-		button.configure(bg = dead_color)
-		alive.remove(coord)
+		self.populate_timer = None
+		self.is_running = False
 
-#Function to clear grid
+		self.buttons = []
+		self.alive = []
 
-def clear():
-	global dead_neighbors_checked
-	global turn_alive
-	global turn_dead
-	global alive
-	dead_neighbors_checked = []
-	turn_alive = []
-	turn_dead = []
-	
-	for coord in alive:
-		button = buttons[coord[0]][coord[1]]
-		button.configure(bg = dead_color)
-	alive = []
+	# Function to manage iterations
 
-	is_clearing = False
+	def _run(self):
+		self.is_running = self.populate()
+		if self.is_running: #repeat if there are still cells, or if stop() or clear() is called
+			self.start()
 
-def clearing_timer():
-	global is_clearing
-	is_clearing = True
-	t = Timer(speed, clear())
-	t.start()
+	# Function to begin iterations
 
-#Functions that implement the game mechanics
+	def start(self):
+		iter_button.configure(text = "Stop", command=lambda: self.stop())
 
-def check_neighbors(r, c):
-	live_neighbors = []
-	global dead_neighbors_checked
-	global turn_alive
+		self.is_running = True
+		self.populate_timer = Timer(self._interval, self._run)
+		self.populate_timer.start()
 
-	for i in [-1,0,1]:
-		for j in [-1,0,1]:
-			coo = [r + i, c + j]
+	# Function to stop populating
 
-			if (i != 0 or j != 0) and (coo in alive):
-				live_neighbors.append(coo)
+	def stop(self):
+		self.populate_timer.cancel()
+		self.is_running = False
 
-			if (i != 0 or j != 0) and (coo not in alive) and (coo not in dead_neighbors_checked):
-				dead_neighbors_checked.append(coo)
+		iter_button.configure(text = "Start", command=lambda: self.start())
 
-				live_neighbors_of_dead = []
-				for m in [-1,0,1]:
-					for n in [-1,0,1]:
-						deadcoo = [coo[0] + m, coo[1] + n]
-						if (m != 0 or n != 0) and (deadcoo in alive):
-							live_neighbors_of_dead.append(deadcoo)
-				num = len(live_neighbors_of_dead)
+	# Function to clear grid and stop populating
 
-				#RULE 4
-				if num == 3:
-					turn_alive.append(coo)
+	def clear(self):
+		if self.is_running:
+			self.stop()
 
-	return len(live_neighbors)
+		for coord in self.alive:
+			button = self.buttons[coord[0]][coord[1]]
+			button.configure(bg = self._dead_color)
 
-def populate():
-	if alive: #if there are any alive
-		global dead_neighbors_checked
-		global turn_alive
-		global turn_dead
-		dead_neighbors_checked = [] #Reset for each time it repopulates
-		turn_alive = []
-		turn_dead = []
+		self.alive = []
 
-		for coord in alive:
-			row = coord[0]
-			col = coord[1]
-			neighbors = check_neighbors(row, col)
+	# Function to change color when a cell is clicked
 
-			#RULE 1
-			if neighbors < 2:
-				turn_dead.append(coord)
-			#RULE 2
-			elif (neighbors == 2) or (neighbors == 3):
-				pass
-			#RULE 3
-			elif neighbors > 3:
-				turn_dead.append(coord)
+	def click(self, row, col):
+		"""
+		Parameters:
+		==========
+		row: row of cell being clicked
+		col: column of cell being clicked
+		"""
 
-		if not is_clearing:
+		button = self.buttons[row][col]
+		coord = [row, col]
+
+		if coord not in self.alive:
+			button.configure(bg = self._live_color)
+			self.alive.append(coord)
+		elif coord in self.alive:
+			button.configure(bg = self._dead_color)
+			self.alive.remove(coord)
+
+	# Function that updates each cell during each refresh cycle
+
+	def populate(self):
+
+		if self.alive:
+			#Reset for each time it repopulates
+			dead_neighbors_checked = []
+			turn_alive = []
+			turn_dead = []
+
+			for coord in self.alive:
+				row = coord[0]
+				col = coord[1]
+				neighbors = self.check_neighbors(row, col, dead_neighbors_checked, turn_alive)
+
+				#RULE 1
+				if neighbors < 2:
+					turn_dead.append(coord)
+				#RULE 2
+				elif (neighbors == 2) or (neighbors == 3):
+					pass
+				#RULE 3
+				elif neighbors > 3:
+					turn_dead.append(coord)
+
 			for coord in turn_alive:
 				row = coord[0]
 				col = coord[1]
-				button = buttons[row][col]
-				button.configure(bg = live_color)
-				alive.append(coord)
+				button = self.buttons[row][col]
+				button.configure(bg = self._live_color)
+				self.alive.append(coord)
 
 			for coord in turn_dead:
 				row = coord[0]
 				col = coord[1]
-				button = buttons[row][col]
-				button.configure(bg = dead_color)
-				alive.remove(coord)
+				button = self.buttons[row][col]
+				button.configure(bg = self._dead_color)
+				self.alive.remove(coord)
 
-	else:
-		print "There are no living cells."
+			return True
 
-		global is_running
-		is_running = False
+		else:
+			print "There are no living cells."
+			iter_button.configure(text = "Start", command=lambda: self.start())
+			return False
 
-		iter_button.configure(text = "Start", command=lambda: IterateController(speed))
+	# Function that checks how many neighbors a cell has
 
-#Start and stop timer controller
+	def check_neighbors(self, r, c, _dead_neighbors_checked, _turn_alive):
+		"""
+		Parameters:
+		==========
+		r: row of cell to check
+		c: column of cell to check
+		_dead_neighbors_checked: memoization list carried from populate() of surrounding dead cells 
+		_turn_alive: list given by populate() to check for those cells to become alive
+		"""
 
-class IterateController:
-	
-	def __init__(self, interval):
-		self._timer = None
-		self.interval = interval
-		self.start()
+		live_neighbors = []
+		for i in [-1,0,1]:
+			for j in [-1,0,1]:
+				coo = [r + i, c + j]
 
-	def _run(self):
-		populate()
-		global is_clearing
-		if is_running and not is_clearing:
-			self.start()
+				if (i != 0 or j != 0) and (coo in self.alive):
+					live_neighbors.append(coo)
 
-	def start(self):
-		global is_running
+				if (i != 0 or j != 0) and (coo not in self.alive) and (coo not in _dead_neighbors_checked):
+					_dead_neighbors_checked.append(coo)
 
-		iter_button.configure(text = "Stop", command=lambda: self.stop())
+					live_neighbors_of_dead = []
+					for m in [-1,0,1]:
+						for n in [-1,0,1]:
+							deadcoo = [coo[0] + m, coo[1] + n]
+							if (m != 0 or n != 0) and (deadcoo in self.alive):
+								live_neighbors_of_dead.append(deadcoo)
+					num = len(live_neighbors_of_dead)
 
+					#RULE 4
+					if num == 3:
+						_turn_alive.append(coo)
 
-		is_running = True
-		self._timer = Timer(self.interval, self._run)
-		self._timer.start()
+		return len(live_neighbors)
 
-	def stop(self):
-		self._timer.cancel()
-		is_running = False
+# Setup GUI
 
-		iter_button.configure(text = "Start", command=lambda: self.start())
-
-
-#Setup initial state
+root = tk.Tk()
+button_container = tk.Frame(root)
+button_container.grid(row = height + 1, columnspan = width, sticky = "wens", padx = 6, pady = 6)
 
 blank_image = tk.PhotoImage()
 button_width = math.ceil(window_width/width)
 button_height = math.ceil(window_height/height)
 
+# Create object and setup initial state
+
+controller = Controller(interval, live_color, dead_color)
+
 for row in range(height):
-	buttons.append([])
+	controller.buttons.append([])
 	for col in range(width):
 		button = tk.Button(root, width = button_width, height = button_height, 
-						    bg = dead_color, image = blank_image, borderwidth = 1,
-                           command=lambda row=row, col=col: click(row, col))
-		buttons[row].append(button)
+						    bg = dead_color, image = blank_image, borderwidth = button_border_width,
+                           command=lambda row=row, col=col: controller.click(row, col))
+		controller.buttons[row].append(button)
 		button.grid(row=row, column=col, sticky="nsew")
 
 iter_button = tk.Button(button_container, text = "Start",
-						command=lambda: IterateController(speed))
+						command=lambda: controller.start())
 iter_button.pack(fill = BOTH, expand = True, side = LEFT)
 
 clear_button = tk.Button(button_container, text = "Clear",
-						command=lambda: clear())
+						command=lambda: controller.clear())
 clear_button.pack(fill = BOTH, expand = True, side = RIGHT)
-
 
 root.mainloop()
